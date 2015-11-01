ae9f81f6c4bddda4104decc87f8a370f12765a25
Eli Courtwright <eli@courtwright.org>
2015-10-31 23:44:15 -0400

    added model check placeholder file

--- bands/__init__.py
+++ bands/__init__.py
@@ -3,6 +3,7 @@ from panels import *
 from bands._version import __version__
 from bands.config import *
 from bands.models import *
+import bands.model_checks
 import bands.automated_emails
 
 static_overrides(join(bands_config['module_root'], 'static'))
---
+++ bands/model_checks.py
@@ -0,0 +1,8 @@
+from bands import *
+
+# This is where we'd put validations for the Band model.  It would look something like
+#
+# @validation.Band
+# def some_validation(band):
+#     if band.whatever == c.SOME_VALUE:
+#         return 'Message shown to the band explaining what they need to do'
