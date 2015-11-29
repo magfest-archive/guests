e8cff00fc7447290c499c04952647e5824401a9e
Eli Courtwright <eli@courtwright.org>
2015-11-29 00:41:50 -0500

    added automated email for initial performer checklist

--- bands/automated_emails.py
+++ bands/automated_emails.py
@@ -2,6 +2,5 @@ from bands import *
 
 AutomatedEmail.extra_models[Band] = lambda session: session.query(Band).options(joinedload(Band.group)).all()
 
-# TODO: fill this in with whatever we need, like if we should email them right away or what
-AutomatedEmail(Band, 'Band Example Email Subject', 'band_notification.txt',
-               lambda b: True, sender=c.BAND_EMAIL)
+AutomatedEmail(Band, '{EVENT_NAME} Performer Checklist', 'band_notification.txt',
+               lambda band: True, sender=c.BAND_EMAIL)
