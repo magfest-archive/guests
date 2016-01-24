ad88c5290e8c7bcaf13d41e8a72082856d935b88
Eli Courtwright <eli@courtwright.org>
2016-01-23 19:03:39 -0500

    added charity, merchandise, and automated reminder emails

--- bands/automated_emails.py
+++ bands/automated_emails.py
@@ -2,5 +2,32 @@ from bands import *
 
 AutomatedEmail.extra_models[Band] = lambda session: session.query(Band).options(joinedload(Band.group)).all()
 
-AutomatedEmail(Band, '{EVENT_NAME} Performer Checklist', 'band_notification.txt',
-               lambda band: True, sender=c.BAND_EMAIL)
+class BandEmail(AutomatedEmail):
+    def __init__(self, *args, **kwargs):
+        if len(args) < 3 and 'filter' not in kwargs:
+            kwargs['filter'] = lambda x: True
+        AutomatedEmail.__init__(self, Band, *args, sender=c.BAND_EMAIL, **kwargs)
+
+
+BandEmail('{EVENT_NAME} Performer Checklist', 'band_notification.txt')
+
+BandEmail('Last chance to apply for a {EVENT_NAME} Panel', 'band_panel_reminder.txt',
+          lambda b: not b.completed_panel and days_before(3, c.BAND_PANEL_DEADLINE))
+
+BandEmail('Last Chance to accept your offer to perform at {EVENT_NAME}', 'band_agreement_reminder.txt',
+          lambda b: not b.completed_agreement and days_before(3, c.BAND_AGREEMENT_DEADLINE))
+
+BandEmail('Last chance to include your bio info on the {EVENT_NAME} website', 'band_bio_reminder.txt',
+          lambda b: not b.completed_bio and days_before(3, c.BAND_BIO_DEADLINE))
+
+BandEmail('{EVENT_NAME} W9 reminder', 'band_w9_reminder.txt',
+          lambda b: b.payment and not b.completed_w9 and days_before(3, c.BAND_W9_DEADLINE))
+
+BandEmail('Last chance to sign up for selling merchandise at {EVENT_NAME}', 'band_merch_reminder.txt',
+          lambda b: not b.merch and days_before(3, c.BAND_MERCH_DEADLINE))
+
+BandEmail('{EVENT_NAME} charity auction reminder', 'band_charity_reminder.txt',
+          lambda b: not b.charity and days_before(3, c.BAND_CHARITY_DEADLINE))
+
+BandEmail('{EVENT_NAME} stage plot reminder', 'band_stage_plot_reminder.txt',
+          lambda b: not b.uploaded_stage_plot and days_before(3, c.STAGE_AGREEMENT_DEADLINE))
