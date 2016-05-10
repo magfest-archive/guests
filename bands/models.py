02b268ba996662e5ab96146af41c4fb45b7cf8cd
Victoria Earl <kitsuta@gmail.com>
2016-05-10 23:30:05 +0200

    Make bands plugin extensible (#23)
    * Break apart schema into one class per step (#17)
    
    * Change schema and convert biography pages
    This is partially-working. Biography information is not actually saved. Committing and pushing for code review.
    
    * Finish updating bands plugin for new schema
    All pages tested as working. Refactor complete.
    
    * Rename columns for less verbose variables
    Now that we've split up the schema, we don't need things like 'band.bio.bio_pic_url'.
    
    * Change wants_panel to ENUM from INT
    The wants_panel Column used an odd workaround before which was unintuitive. A set of options is more idiomatic. Fixes #19.
    
    * Use getattr to avoid CSV output failure
    Fixes #21, albeit in a clunky way.
    
    * Add Band completed() func for automated emails
    Now that the "completed" property is underneath each step, and the step for a band doesn't exist until the band completes that step, we need a way for the automated email script to check if a band has completed a step without erroring. This adds that function.
    
    * Use deadlines to hide/show steps (#22)
    
    Fixes #13 and fixes #14 by simply showing steps only if their deadline is set. Also adds a bandextra.html for adding more steps.

--- bands/models.py
+++ bands/models.py
@@ -18,118 +18,168 @@ class Event:
 class Band(MagModel):
     group_id = Column(UUID, ForeignKey('group.id'))
     event_id = Column(UUID, ForeignKey('event.id'), nullable=True)
+    info = relationship('BandInfo', backref=backref('band', load_on_pending=True), uselist=False)
+    bio = relationship('BandBio', backref=backref('band', load_on_pending=True), uselist=False)
+    taxes = relationship('BandTaxes', backref=backref('band', load_on_pending=True), uselist=False)
+    stage_plot = relationship('BandStagePlot', backref=backref('band', load_on_pending=True), uselist=False)
+    panel = relationship('BandPanel', backref=backref('band', load_on_pending=True), uselist=False)
+    merch = relationship('BandMerch', backref=backref('band', load_on_pending=True), uselist=False)
+    charity = relationship('BandCharity', backref=backref('band', load_on_pending=True), uselist=False)
 
     payment = Column(Integer, default=0, admin_only=True)
     vehicles = Column(Integer, default=1, admin_only=True)
     estimated_loadin_minutes = Column(Integer, default=60, admin_only=True)
     estimated_performance_minutes = Column(Integer, default=60, admin_only=True)
 
-    poc_phone = Column(UnicodeText)
-    performer_count = Column(Integer, default=0)
-    bringing_vehicle = Column(Boolean, default=False)
-    vehicle_info = Column(UnicodeText)
-    arrival_time = Column(UnicodeText)
-
-    bio = Column(UnicodeText)
-    website = Column(UnicodeText)
-    facebook = Column(UnicodeText)
-    twitter = Column(UnicodeText)
-    other_social_media = Column(UnicodeText)
-
-    w9_filename = Column(UnicodeText)
-    w9_content_type = Column(UnicodeText)
-
-    bio_pic_filename = Column(UnicodeText)
-    bio_pic_content_type = Column(UnicodeText)
-
-    stage_plot_filename = Column(UnicodeText)
-    stage_plot_content_type = Column(UnicodeText)
-
-    # This needs to be a nullable integer rather than a nullable boolean to prevent SQLAlchemy from setting a False value
-    # when it's instantiated and saved without this field being set.  An annoying but necessary workaround.
-    wants_panel = Column(Integer, nullable=True, default=None)
-    panel_name = Column(UnicodeText)
-    panel_length = Column(UnicodeText)
-    panel_desc = Column(UnicodeText)
-    panel_tech_needs = Column(MultiChoice(c.TECH_NEED_OPTS))
-
-    merch = Column(Choice(c.BAND_MERCH_OPTS), nullable=True)
-
-    charity = Column(Choice(c.BAND_CHARITY_OPTS), nullable=True)
-    charity_donation = Column(UnicodeText)
-
     @property
-    def email(self):
-        return self.group.email
+    def all_badges_claimed(self):
+        return not any(a.is_unassigned for a in self.group.attendees)
 
     @property
     def estimated_performer_count(self):
-        return len([a for a in self.group.attendees if a.badge_type == c.GUEST_BADGE])
+        return len([a for a in self.group.attendees if a.badge_type == c.GUEST_BADGE]) or 0
 
     @property
     def performance_minutes(self):
         return self.event.minutes if self.event_id else self.estimated_performance_minutes
 
     @property
+    def email(self):
+        return self.group.email
+
+    def completed(self, model):
+        """
+        This is a safe way to check if a step has been completed for a particular band. It checks for a custom
+        'completed' property for the step; if that doesn't exist, it will attempt to return an ID of the step.
+
+        :param model: This should match one of the relationship columns in the Band class, e.g., 'bio' or 'stage_plot'
+        :return: Returns either the 'completed' property of the model, the model's ID for that band, or None.
+        """
+
+        subclass = getattr(self, model)
+        return getattr(subclass, 'completed', getattr(subclass, 'id')) if subclass else None
+
+
+class BandInfo(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    poc_phone = Column(UnicodeText)
+    performer_count = Column(Integer, default=0)
+    bringing_vehicle = Column(Boolean, default=False)
+    vehicle_info = Column(UnicodeText)
+    arrival_time = Column(UnicodeText)
+
+    @property
+    def completed(self):
+        return self.poc_phone
+
+
+class BandBio(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    desc = Column(UnicodeText)
+    website = Column(UnicodeText)
+    facebook = Column(UnicodeText)
+    twitter = Column(UnicodeText)
+    other_social_media = Column(UnicodeText)
+
+    pic_filename = Column(UnicodeText)
+    pic_content_type = Column(UnicodeText)
+
+    @property
+    def pic_url(self):
+        return '{}/bands/view_bio_pic?id={}'.format(c.URL_BASE, self.band.id) if self.uploaded_pic else ''
+
+    @property
+    def pic_fpath(self):
+        return os.path.join(bands_config['root'], 'uploaded_files', 'bio_pics', self.id)
+
+    @property
+    def uploaded_pic(self):
+        return os.path.exists(self.pic_fpath)
+
+    @property
+    def pic_extension(self):
+        return extension(self.pic_filename)
+
+    @property
+    def completed(self):
+        return self.desc
+
+
+class BandTaxes(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    w9_filename = Column(UnicodeText)
+    w9_content_type = Column(UnicodeText)
+
+    @property
     def w9_url(self):
-        return '{}/bands/view_w9?id={}'.format(c.URL_BASE, self.id) if self.completed_w9 else ''
-
-    @property
-    def bio_pic_url(self):
-        return '{}/bands/view_bio_pic?id={}'.format(c.URL_BASE, self.id) if self.uploaded_bio_pic else ''
-
-    @property
-    def stage_plot_url(self):
-        return '{}/bands/view_stage_plot?id={}'.format(c.URL_BASE, self.id) if self.uploaded_stage_plot else ''
+        return '{}/bands/view_w9?id={}'.format(c.URL_BASE, self.band.id) if self.completed_w9 else ''
 
     @property
     def w9_fpath(self):
         return os.path.join(bands_config['root'], 'uploaded_files', 'w9_forms', self.id)
 
     @property
-    def bio_pic_fpath(self):
-        return os.path.join(bands_config['root'], 'uploaded_files', 'bio_pics', self.id)
-
-    @property
-    def uploaded_bio_pic(self):
-        return os.path.exists(self.bio_pic_fpath)
-
-    @property
-    def stage_plot_fpath(self):
-        return os.path.join(bands_config['root'], 'uploaded_files', 'stage_plots', self.id)
-
-    @property
-    def uploaded_stage_plot(self):
-        return os.path.exists(self.stage_plot_fpath)
-
-    @property
     def w9_extension(self):
         return extension(self.w9_filename)
 
     @property
-    def bio_pic_extension(self):
-        return extension(self.bio_pic_filename)
+    def completed(self):
+        return self.w9_filename
+
+
+class BandStagePlot(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    filename = Column(UnicodeText)
+    content_type = Column(UnicodeText)
+
+    @property
+    def url(self):
+        return '{}/bands/view_stage_plot?id={}'.format(c.URL_BASE, self.band.id) if self.uploaded_file else ''
+
+    @property
+    def fpath(self):
+        return os.path.join(bands_config['root'], 'uploaded_files', 'stage_plots', self.id)
+
+    @property
+    def uploaded_file(self):
+        return os.path.exists(self.fpath)
 
     @property
     def stage_plot_extension(self):
-        return extension(self.stage_plot_filename)
+        return extension(self.filename)
 
     @property
-    def all_badges_claimed(self):
-        return not any(a.is_unassigned for a in self.group.attendees)
+    def completed(self):
+        return self.uploaded_file
+
+
+class BandPanel(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    wants_panel = Column(Choice(c.BAND_PANEL_OPTS), nullable=True)
+    name = Column(UnicodeText)
+    length = Column(UnicodeText)
+    desc = Column(UnicodeText)
+    tech_needs = Column(MultiChoice(c.TECH_NEED_OPTS))
 
     @property
-    def completed_agreement(self):
-        return bool(self.poc_phone)
+    def completed(self):
+        return self.wants_panel
+
+
+class BandMerch(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    selling_merch = Column(Choice(c.BAND_MERCH_OPTS), nullable=True)
 
     @property
-    def completed_bio(self):
-        return bool(self.bio)
+    def completed(self):
+        return self.selling_merch
+
+
+class BandCharity(MagModel):
+    band_id = Column(UUID, ForeignKey('band.id'), unique=True)
+    donating = Column(Choice(c.BAND_CHARITY_OPTS), nullable=True)
+    desc = Column(UnicodeText)
 
     @property
-    def completed_w9(self):
-        return os.path.exists(self.w9_fpath)
-
-    @property
-    def completed_panel(self):
-        return self.wants_panel is not None
+    def completed(self):
+        return self.donating
