15bcb37dbe00d551d663fc6420e14b2a41fb3e63
Rob Ruana <rob@robruana.com>
2017-08-18 12:22:31 -0400

    Fixes re-upload of band W9 form

--- bands/site_sections/bands.py
+++ bands/site_sections/bands.py
@@ -59,8 +59,9 @@ class Root:
             'message': message
         }
 
-    def w9(self, session, band_id=None, id=None, message='', w9=None, **params):
-        band_id = band_id or id
+    def w9(self, session, band_id=None, message='', w9=None, **params):
+        if not band_id:
+            band_id = params.pop('id', None)
         assert band_id, 'Either a band_id or id is required'
         band = session.band(band_id)
         band_taxes = session.band_taxes(params, restricted=True)
