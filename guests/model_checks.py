4cdacea3f137372bf21fb314a7d092ec72e73d23
Rob Ruana <rob@robruana.com>
2017-10-05 10:57:46 -0400

    Removes empty inventory validation check

--- guests/model_checks.py
+++ guests/model_checks.py
@@ -7,10 +7,7 @@ def is_merch_checklist_complete(guest_merch):
         return 'You need to tell us whether and how you want to sell merchandise'
 
     elif guest_merch.selling_merch == c.ROCK_ISLAND:
-        if not guest_merch.inventory:
-            return 'You must add some merch to your inventory!'
-
-        elif not guest_merch.poc_is_group_leader and not (
+        if not guest_merch.poc_is_group_leader and not (
                 guest_merch.poc_first_name and
                 guest_merch.poc_last_name and
                 guest_merch.poc_phone and
